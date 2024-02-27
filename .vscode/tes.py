0a36779ecdef1745bed801cf07f02aeeb095aa8e
devanggafisdiovany <devanggafisdiovany29@gmail.com>
2024-02-27 15:21:19 +0700

    Commit pertamaku 2024

---
+++ .vscode/tes.py
@@ -0,0 +1,8 @@
+print("Halo Sobat Inners!")
+
+nama = "Devangga Fisdiovany"
+umur = "21"
+pekerjaan = "Mahasiswa"
+
+print("Perkenalkan! aku " + nama + ", seorang " + pekerjaan + " berumur " + umur + " tahun ")
+print("Terima kasih!")
